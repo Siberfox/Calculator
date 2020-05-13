07fc0af63a70de65c096804b0d68d7b7e775b477
Siberfox <greyfoxjob@gmail.com>
2020-05-13 11:43:29 +0600

    update bundle

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: { main: './index.js' },
+  entry: { index: './index.js' },
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
